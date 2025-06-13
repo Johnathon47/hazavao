754905a95ca4f3e8ce1d2989dc22ace20741b048
Johnathon47 <hei.johnathon.2@gmail.com>
2025-06-13 09:26:03 +0300

    feat: juste return hello + 'teny'(word enter on param)

--- src/main/java/com/my/hei/service/HazavaoService.java
+++ src/main/java/com/my/hei/service/HazavaoService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class HazavaoService {
   public String getDefinition(String teny) {
-    return "rien d'abord";
+    return "Hello " + teny;
   }
 }
