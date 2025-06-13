359d10f519b577d422cbab2f6b4bd697904171ee
Johnathon47 <hei.johnathon.2@gmail.com>
2025-06-13 09:11:31 +0300

    feat: add controller for hazavao

---
+++ src/main/java/com/my/hei/endpoint/rest/controller/health/HazavaoController.java
@@ -0,0 +1,20 @@
+package com.my.hei.endpoint.rest.controller.health;
+
+import com.my.hei.service.HazavaoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HazavaoController {
+  private HazavaoService hazavaoService;
+
+  public HazavaoController(HazavaoService hazavaoService) {
+    this.hazavaoService = hazavaoService;
+  }
+
+  @GetMapping("/hazavao")
+  public String hazavao(@RequestParam String teny) {
+    return "rien pour l'instant";
+  }
+}
