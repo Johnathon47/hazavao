b6c1c6236733c72388a0bbb6f2eaf20f6e802795
Johnathon47 <hei.johnathon.2@gmail.com>
2025-06-13 09:11:49 +0300

    feat: add service for hazavao

---
+++ src/main/java/com/my/hei/service/HazavaoService.java
@@ -0,0 +1,10 @@
+package com.my.hei.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HazavaoService {
+  public String getDefinition(String teny) {
+    return "rien d'abord";
+  }
+}
